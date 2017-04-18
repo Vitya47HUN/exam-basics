68b8df5d97ab24219a4bd50fdc91c61ce7d33092
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-18 09:27:37 +0200

    First exercise started, created a function

---
+++ Task01Oddavg/src/Oddavg.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Oddavg {
+  public static void main(String[] args) {
+    // Create a function called `oddAverage` that takes a list of numbers as parameter
+    // and returns the average value of the odd numbers in the list
+    // Create basic unit tests for it with at least 3 different test cases
+  }
+
+  public void oddAverage(List<Integer>){
+
+  }
+
+}
