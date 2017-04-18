3d640a15cf428453f7ca99f6a3f727d3bead42a1
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-18 09:36:31 +0200

    First exercise, function is almost ready, needs to be tested

--- Task01Oddavg/src/Oddavg.java
+++ Task01Oddavg/src/Oddavg.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Oddavg {
   public static void main(String[] args) {
+    ArrayList<Integer> numList = new ArrayList<>();
+    numList.add(1);
+    numList.add(2);
+    numList.add(3);
     // Create a function called `oddAverage` that takes a list of numbers as parameter
     // and returns the average value of the odd numbers in the list
     // Create basic unit tests for it with at least 3 different test cases
+    
   }
 
-  public void oddAverage(List<Integer>){
-
+  public void oddAverage(ArrayList<Integer> input){
+    ArrayList<Integer> sortedList = new ArrayList<>();
+    for (int i = 0;i < input.size(); i++){
+      if(input.indexOf(i) % 2 != 0){
+        input.add(i);
+      }
+    }
+    System.out.println(input);
   }
 
 }
