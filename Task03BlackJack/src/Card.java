c52e5a4d44bf2af6dceeb9d0331acc8e1088e6ad
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-18 13:13:43 +0200

    Trial03 card class finished, deck class needs a draw function

--- Task03BlackJack/src/Card.java
+++ Task03BlackJack/src/Card.java
@@ -1,9 +1,18 @@
 public class Card {
+  public static final String COLOR_HEART = "heart";
   String color;
-  String  value;
+  int value;
 
-  Card(String color,String value){
+  public Card(String color, int value) {
     this.color = color;
-    this.value = value;
+    if (value > 4) {
+      this.value = 4;
+    } else {
+      this.value = value;
+    }
+  }
+
+  public int getValue() {
+    return value;
   }
 }
--- Task03BlackJack/src/Deck.java
+++ Task03BlackJack/src/Deck.java
@@ -1,10 +1,19 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
-  int numOfCards;
-  public ArrayList<Card> cards = new ArrayList<>();
+  private static final String[] COLORS = new String[] {"hearts","clubs","diamonds","spades" };
+  ArrayList<Card> cards;
+  ArrayList<Card> used;
 
-  Deck(int numOfCards){
-    this.numOfCards = numOfCards;
+  public Deck() {
+    cards = new ArrayList<>();
+    for (String color : COLORS) {
+      for (int i = 1; i < 14; i++) {
+        cards.add(new Card(color, i));
+      }
+    }
+    used = new ArrayList<>();
+    Collections.shuffle(cards);
   }
 }
