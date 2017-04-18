b57d21110fed12c2c58a0f82ea84c7e0b9b16f98
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-18 12:12:52 +0200

    Copy function still not working,cant print out the content of the file

--- Task02Copy/src/Copy.java
+++ Task02Copy/src/Copy.java
@@ -7,8 +7,10 @@ import java.util.List;
 
 
 public class Copy {
-  public static void main(String[] args) throws IOException {
-//    List<String> todoLines = readLinesFromFile();
+  public static void main(String[] args){
+    File source = new File(args[1]);
+    File destination = new File(args[2]);
+    List<String> Lines = readLinesFromFile(source);
     // This should be the basic replica of the 'cp' command
     // If ran from the command line without arguments
     // It should print out the usage:
@@ -23,9 +25,38 @@ public class Copy {
     } else if (args.length == 1) {
       Usage.oneArgument();
     } else if (args.length == 3 && args[0] == "copy") {
-      File source = new File(args[1]);
-      File destination = new File(args[2]);
-      Usage.copyFile(source,destination);
+      try {
+//        File source = new File(args[1]);
+//        File destination = new File(args[2]);
+        Files.copy( source.toPath(), destination.toPath());
+        for (int i = 0; i < Lines.size(); i++) {
+          System.out.println(i + 1 + " - " + Lines.get(i));
+          writeToFile(Lines,source);
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private static List<String> readLinesFromFile(File input) {
+    Path path = Paths.get(String.valueOf(input));
+    List<String> todoLines;
+    try {
+      todoLines = Files.readAllLines(path);
+    } catch (IOException e) {
+      e.printStackTrace();
+      todoLines = new ArrayList<>();
+    }
+    return todoLines;
+  }
+
+  private static void writeToFile(List<String> data,File input) {
+    Path path = Paths.get(String.valueOf(input));
+    try {
+      Files.write(path, data);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 
